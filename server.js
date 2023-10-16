c84b778a3f3551984f23873a3759cca5feedb3e2
范綉莉 <shakira.fang@secom.com.tw>
2023-10-16 15:51:32 +0800

    backend api

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const cors =require('cors');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRoutes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(5004,console.log("server started"));
+mongoose
+  .connect("mongodb://localhost:27017/netflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  });
+
+app.use("/api/user",userRoutes);
+
+app.listen(5004, console.log("server started"));
